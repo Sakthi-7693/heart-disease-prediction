c129d50ff76319e03aae1d6b824cf98b056fe7b4
Sakthi <sakthi.1617s@gmail.com>
2024-10-06 00:48:27 +0530

    Test purpose for Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
+import pandas as pd
+
+import sklearn as sk
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
 
-st.write("Welcome Sakthi")
+st.write("Welcome Sakthi 123")
